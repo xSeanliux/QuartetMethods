fa91368478c1110517d124d0cfe68e2a631913ad
Sean Liu <zxliu2@illinois.edu>
2024-02-29 11:59:21 -0600

    run_inference other methods now use IDs

--- run_inference.sh
+++ run_inference.sh
@@ -9,9 +9,10 @@ DO_HEURISTIC_ASTRAL=false # h(euristic)
 USE_MORPH=false  # m(orphology)
 TARGETTREES=./QuartetMethods/example/trees_small.txt
 FACTORS=(0.5 1.0 2.0 4.0 8.0) # f
+SETTINGS=(low mod modhigh high veryhigh) # s 
 RUNID=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo) # random string so that runs don't use the same name (i.e. for temp files)
 MB_EXEC=/projects/tallis/zxliu2/bin/bin/mb
-while getopts 'apghmf::' o; do 
+while getopts 'apghmf:' o; do 
     echo $o' '$OPTARG
     case $o in 
         a) DO_ASTRAL=true;;
@@ -21,17 +22,20 @@ while getopts 'apghmf::' o; do
         h) DO_HEURISTIC_ASTRAL=true;;
         f) 
         if [[ ${FACTORS[@]} =~ $OPTARG ]]; then  
-            echo "FOUND" 
+            echo "FOUND FACTOR "$OPTARG 
             FACTORS=($OPTARG)
         else 
-            echo "NOT FOUND"
+            echo "FACTOR NOT FOUND. USING ALL"
         fi;;
     esac
 done 
 
+echo "ASTRAL_HEURISTIC $DO_HEURISTIC_ASTRAL"
 echo "ASTRAL: $DO_ASTRAL"
 echo "MP4: $DO_MP4"
 echo "GA: $DO_GA"
+echo "Settings:"${SETTINGS[@]}
+echo "Factors:"${FACTORS[@]}
 
 if [[ $OS_TYPE = "RedHat" ]]; then 
     PAUP_PATH=./QuartetMethods/scripts/paup4a168_centos64
@@ -77,8 +81,8 @@ for f in ${FACTORS[@]}; do
         mkdir -p $TREEOUTPUT/GA/scores
         >$GA_SCOREOUTPUT # set up score output
     fi
-    touch quartet_temp.txt
-    touch nexus_temp.txt
+    touch tmp_quartet_$RUNID.txt
+    touch tmp_nexus_$RUNID.txt
     SETTINGS=(low mod modhigh high veryhigh)
     for setting in ${SETTINGS[@]}; do
         CSVS=$DATASET/$setting'_noborrowing/no-morph'
@@ -97,20 +101,20 @@ for f in ${FACTORS[@]}; do
                         echo "Factor: $f; ID = $id: target is tree $i"
                         # generate quartets
                         if $DO_GA; then 
-                            >mb_tmp.nex
-                            Rscript ./QuartetMethods/scripts/commandLineNex.R -f $FILE -o mb_tmp.nex --resolve-poly 4 --morph-weight 1.0 > /dev/null 2> /dev/null
+                            >tmp_mb_$RUNID.nex
+                            Rscript ./QuartetMethods/scripts/commandLineNex.R -h $RUNID -f $FILE -o tmp_mb_$RUNID.nex --resolve-poly 4 --morph-weight 1.0 > /dev/null 2> /dev/null
                             echo "✅ GA nexus files"
-                            $MB_EXEC mb_tmp.nex > mb_tmp_out.txt 2> mb_tmp_run.txt
+                            $MB_EXEC tmp_mb_$RUNID.nex > tmp_mb_out_$RUNID.txt 2> tmp_mb_log_$RUNID.txt
                             echo "✅ GA sampling"
-                            mv Bayes_out.con.tre $TREEOUTPUT/GA/trees/$id.trees
-                            mv Bayes_out.t $TREEOUTPUT/GA/trees1/$id.trees 
-                            rm Bayes_out.* mb_tmp*
+                            mv Bayes_out_$RUNID.con.tre $TREEOUTPUT/GA/trees/$id.trees
+                            mv Bayes_out_$RUNID.t $TREEOUTPUT/GA/trees1/$id.trees 
+                            rm Bayes_out_$RUNID.* mb_tmp*
                         fi
                         if $DO_MP4; then 
-                            >nexus_temp.nex
-                            Rscript ./QuartetMethods/scripts/commandLineNex.R -f $FILE -o mp4_nexus_temp.nex -p 3 -m 1.0 > /dev/null 2> /dev/null
+                            >tmp_mp4_$RUNID.nex
+                            Rscript ./QuartetMethods/scripts/commandLineNex.R -f $FILE -o tmp_mp4_$RUNID.nex -p 3 -m 1.0 > /dev/null 2> /dev/null
                             echo "✅ MP4 nexus files"
-                            $PAUP_PATH -n mp4_nexus_temp.nex > /dev/null 2> /dev/null
+                            $PAUP_PATH -n tmp_mp4_$RUNID.nex > /dev/null 2> /dev/null
                             mv paup_out.trees $TREEOUTPUT/MP4/trees/$id.trees # If we run lots of instances of this script in parallel, paup_out might be overwritten so we can't have that
                             mv paup_out.scores $TREEOUTPUT/MP4/scores/$id.scores
                             echo "✅ MP4 tree inference" 
@@ -121,17 +125,17 @@ for f in ${FACTORS[@]}; do
                             rm mp4_nexus_temp.nex
                         fi
                         if $DO_ASTRAL || $DO_HEURISTIC_ASTRAL; then # run ASTRAL 
-                            python -c "from QuartetMethods.scripts.getQuartets import *; print_quartets('$FILE')" > quartet_temp.txt
+                            python -c "from QuartetMethods.scripts.getQuartets import *; print_quartets('$FILE')" > tmp_quartet_$RUNID.txt
                             echo "✅ ASTRAL quartet generation" 
 
                             if $DO_ASTRAL; then 
-                                java -jar ./QuartetMethods/ASTRAL/astral.5.7.8.jar -i quartet_temp.txt -o $TREEOUTPUT/ASTRAL/trees/$id.tre -x > /dev/null 2> $TREEOUTPUT/ASTRAL/logs/$id.log # Run ASTRAL in exact mode
+                                java -jar ./QuartetMethods/ASTRAL/astral.5.7.8.jar -i tmp_quartet_$RUNID.txt -o $TREEOUTPUT/ASTRAL/trees/$id.tre -x > /dev/null 2> $TREEOUTPUT/ASTRAL/logs/$id.log # Run ASTRAL in exact mode
                                 echo "✅ ASTRAL tree inference" 
 
                                 echo $FILE >> $ASTRAL_SCOREOUTPUT
                                 Rscript ./QuartetMethods/scripts/QuartetScorer.R -f newick -r $CURRENT_TREE -m 1 -p 0 -i $TREEOUTPUT/ASTRAL/trees/$id.tre >> $ASTRAL_SCOREOUTPUT
                                 echo "✅ ASTRAL tree scoring" 
-                                rm quartet_temp.txt
+                                rm tmp_quartet_$RUNID.txt
                             fi
 
                             if $DO_HEURISTIC_ASTRAL; then 
@@ -139,17 +143,16 @@ for f in ${FACTORS[@]}; do
                                     echo "❌ Heuristic ASTRAL NEXUS file not found: "$TREEOUTPUT/GA/trees1/$id.trees
                                     exit 0
                                 fi
-                                python -c "from QuartetMethods.scripts.getQuartets import *; nexus_to_newick('$TREEOUTPUT/GA/trees1/$id.trees')" > bipartitions.bootstrap.trees
+                                python -c "from QuartetMethods.scripts.getQuartets import *; nexus_to_newick('$TREEOUTPUT/GA/trees1/$id.trees')" > tmp_bipartitions_$RUNID.bootstrap.trees
                                 echo "✅ Heuristic ASTRAL NEXUS to Newick" 
-                                java -jar ./QuartetMethods/ASTRAL/astral.5.7.8.jar -i quartet_temp.txt -o $TREEOUTPUT/ASTRAL-H/trees/$id.tre -e bipartitions.bootstrap.trees > /dev/null 2> $TREEOUTPUT/ASTRAL-H/logs/$id.log # Run ASTRAL in exact mode
+                                java -jar ./QuartetMethods/ASTRAL/astral.5.7.8.jar -i tmp_quartet_$RUNID.txt -o $TREEOUTPUT/ASTRAL-H/trees/$id.tre -e tmp_bipartitions_$RUNID.bootstrap.trees > /dev/null 2> $TREEOUTPUT/ASTRAL-H/logs/$id.log # Run ASTRAL in exact mode
                                 echo "✅ Heuristic ASTRAL tree inference" 
 
                                 echo $FILE >> $ASTRAL_H_SCOREOUTPUT
                                 Rscript ./QuartetMethods/scripts/QuartetScorer.R -f newick -r $CURRENT_TREE -m 1 -p 0 -i $TREEOUTPUT/ASTRAL-H/trees/$id.tre >> $ASTRAL_H_SCOREOUTPUT
                                 echo "✅ Heuristic ASTRAL tree scoring" 
-                                rm quartet_temp.txt
-                                rm bipartitions.bootstrap.trees
-                                exit 0
+                                rm tmp_quartet_$RUNID.txt
+                                rm tmp_bipartitions_$RUNID.bootstrap.trees
                             fi
                         fi
                     fi
@@ -157,6 +160,6 @@ for f in ${FACTORS[@]}; do
             done
         done
     done
-    rm quartet_temp.txt
-    rm nexus_temp.txt
+    rm tmp_quartet_$RUNID.txt
+    rm tmp_nexus_$RUNID.txt
 done
